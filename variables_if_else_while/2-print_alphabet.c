1cc77583e08153a0857b24ceedd09bf28868018f
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-06-18 11:10:38 +0200

    add 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - coucou
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
