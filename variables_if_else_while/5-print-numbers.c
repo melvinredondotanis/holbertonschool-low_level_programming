262423142d1b8ba777ddd0451c354b72ea15e176
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-06-18 11:53:06 +0200

    add ex5

---
+++ variables_if_else_while/5-print-numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - coucou
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
