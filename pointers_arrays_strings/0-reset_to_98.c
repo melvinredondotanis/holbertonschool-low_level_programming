b5f9e3f5e1d7ab5a39e346f75cfc7431170c2e53
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-06-25 10:47:26 +0200

    add main.h + 0 for pointer ans arrays strings

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset to 98
+ * Description: change input value to 98
+ * @n: input value
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
