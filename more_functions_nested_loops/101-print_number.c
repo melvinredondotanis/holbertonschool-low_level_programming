b31ae4d272bf75de624238a6d38cf75ec1f41468
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-06-25 09:26:36 +0200

    clean + new 101

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
@@ -9,21 +8,41 @@
 
 void print_number(int n)
 {
-	if (n == sizeof(int))
-		write(1, "-32767", 6);
+	int i, j, digit, digits, power;
+	unsigned int temp, numchar, number;
+
+	digit = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
 	else
 	{
-		if (n < 0)
-		{
-			n *= -1;
-			_putchar('-');
-		}
-		if (n > 9)
-		{
-			print_number(n / 10);
-			print_number(n % 10);
-		}
-		else
-			_putchar('0' + n);
+		temp = n;
+	}
+
+	number = temp;
+
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	i = 1;
+
+	while (i < digits)
+	{
+		power = power * 10;
+		i++;
+	}
+	j = power;
+	while (j >= 1)
+	{
+		numchar = (temp / j) % 10;
+		_putchar(numchar + '0');
+		j = j / 10;
 	}
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -23,11 +23,6 @@ void print_square(int size);
 
 void print_triangle(int size);
 
-int isPrime(long int n);
-
-long int biggestFactor(long int a);
-
 void print_number(int n);
 
-int numDigits(int n);
 #endif
