9d29ba9751a84ba7e0287bec9b5229c4387a7b4c
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-06-26 13:30:17 +0200

    tkt

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@
 int _atoi(char *str)
 {
 	int i = 0;
-	int value = 0;
+	unsigned int value = 0;
 	int p_or_n = 1;
 
 	while ((str[i] < '0') || (str[i] > '9'))
@@ -21,10 +21,12 @@ int _atoi(char *str)
 			p_or_n *= -1;
 		i++;
 	}
+
+	value *= p_or_n;
 	while ((str[i] >= '0') && (str[i] <= '9'))
 	{
 		value = value * 10 + (str[i] - '0');
 		i++;
 	}
-	return (p_or_n * value);
+	return (value);
 }
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int nb;
 
-	nb = _atoi("2147483640 + 55");
+	nb = _atoi("-2147483648");
 	printf("%d\n", nb);
 	nb = _atoi("29");
 	printf("%d\n", nb);
