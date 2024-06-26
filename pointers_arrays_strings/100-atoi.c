a9faa04131129b113341c9be382c10cbbcbf3dc6
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-06-26 12:07:18 +0200

    j'ai la dalle

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half of a string
+ * @str: char array string type
+ * Description: If odd number of chars, print (length - 1) / 2
+ */
+
+int _atoi(char *str)
+{
+	int i = 0;
+	int value = 0;
+	int p_or_n = 1;
+
+	while ((str[i] < '0') || (str[i] > '9'))
+	{
+		if (str[i] == '\0')
+			return (0);
+		if (str[i] == '+')
+			p_or_n *= 1;
+		else if (str[i] == '-')
+			p_or_n *= -1;
+		i++;
+	}
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		value = value * 10 + str[i] - '0';
+		i++;
+	}
+	return (p_or_n * value);
+}
