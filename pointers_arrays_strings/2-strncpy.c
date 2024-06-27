438816aa680cb358b5d6f6adf04d01a8deb0020c
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-06-27 14:36:05 +0200

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while ((dest[i] || src[i]) && i < n)
+	while ((dest[i] && src[i]) && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-
-	if (src[i] == '\0')
-		dest[i++] = '\0';
 	return (dest);
 }
