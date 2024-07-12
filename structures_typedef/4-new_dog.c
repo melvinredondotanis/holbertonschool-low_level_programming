4448e4a3943155dcab9f7dbd55779dd393b151e0
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-07-12 16:24:07 +0200

    add

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,67 +1,34 @@
 #include "dog.h"
 
 /**
- * _strdup - description of template
- * @str: input value
- *
- * Return: pointer
- */
-char *_strdup(char *str)
-{
-	unsigned int i = 0;
-	unsigned int k = 0;
-	char *ptr;
-
-	if (!str)
-		return (NULL);
-
-	while (str[i])
-		i++;
-
-	ptr = (char *)malloc(sizeof(char) * i + 1);
-
-	if (!ptr)
-		return (NULL);
-
-	while (k <= i)
-	{
-		ptr[k] = str[k];
-		k++;
-	}
-
-	return (ptr);
-}
-
-/**
- * new_dog - creates a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * new_dog - entry point
+ * @name: string from main, name of pet
+ * @age: number from main, age of pet
+ * @owner: string from main, owner of pet
+ * Return: p
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-
-	new_dog = malloc(sizeof(struct dog));
-
-	if (new_dog == 0 || name == 0 || owner == 0)
+	dog_t *p;
+	/* reserving memory to struct*/
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
 		return (NULL);
-
-	new_dog->name = _strdup(name);
-	if (new_dog->name == 0)
+	/* Cpunting name pointer*/
+	if (name == NULL)
 	{
-		free(new_dog);
+		free(p);
+		free(owner);
 		return (NULL);
 	}
-	new_dog->age = age;
-	new_dog->owner = _strdup(owner);
-	if (new_dog->owner == 0)
+	if (owner == NULL)
 	{
-		free(new_dog);
+		free(p);
+		free(name);
 		return (NULL);
 	}
-	return (new_dog);
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
