d6ec93a6ef6f293be1106dcd312e25123d0366ad
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-07-12 15:24:18 +0200

    corrige prt dog

--- structures_typedef/2-print-dog.c
+++ structures_typedef/2-print-dog.c
@@ -8,16 +8,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != 0)
+	if (d != NULL)
 	{
-		if ((*d).name != 0)
-			printf("Name: %s\n", (*d).name);
-		else
-			printf("Name: (nil)\n");
-		printf("Age: %f\n", (*d).age);
-		if ((*d).owner != 0)
-			printf("Owner: %s\n", (*d).owner);
-		else
-			printf("Owner: (nil)\n");
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
