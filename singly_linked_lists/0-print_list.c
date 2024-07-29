2ee5af41deec3a2a34d59b1d2cb0d33ec35dd829
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-07-29 16:34:14 +0200

    add 0 to 2

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
