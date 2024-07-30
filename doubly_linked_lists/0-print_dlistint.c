a533ee243c4a682da9ed97f8ffa41a563befd7c9
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-07-30 14:00:24 +0200

    add 0 for double linked links

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+/*
+ * File: 0-print_dlistint.c
+ * Auth: Melvin Redondo--Tanis
+ */
+
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The dlistint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->n)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+		i++;
+	}
+	return (i);
+}
