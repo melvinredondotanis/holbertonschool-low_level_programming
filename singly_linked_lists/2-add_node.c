81a1bb3a12df82803e4a6adc98f86f5449af15e3
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-07-30 14:52:28 +0200

    corrige

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -21,7 +21,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (head == NULL)
 		return (NULL);
 
-	malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
